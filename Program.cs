6870ae007630c2da6d899f1319bbcd74d4bb52f4
creewick <creewick@ya.ru>
2018-03-19 12:13:53 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace theta_bot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var bot = new ThetaBot("452741789:AAHCMogCOadheB55ZD84k0zLWaD4nyB62c0");
+        }
+    }
+}
---
+++ ThetaBot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace theta_bot
+{
+    public class ThetaBot
+    {
+        private readonly TelegramBotClient Bot;
+
+        public ThetaBot(string token)
+        {
+            Bot = new TelegramBotClient(token);
+
+            Bot.OnMessage += OnMessageReceive;
+            
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        private async void OnMessageReceive(object sender, MessageEventArgs args)
+        {
+            var message = args.Message;
+            await Bot.SendTextMessageAsync(message.Chat.Id, "42");
+        }
+    }
+}
