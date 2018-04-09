a17ca849b997e109014627304f6e6822da3321b3
creewick <creewick@ya.ru>
2018-04-09 11:45:02 +0500

    firebase

---
+++ theta-bot/Database/FirebaseProvider.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Firebase.Database;
+using Firebase.Database.Query;
+
+namespace theta_bot
+{
+    public class FirebaseProvider : IDataProvider
+    {
+        private readonly FirebaseClient Database;
+        
+        public FirebaseProvider(string url, string token)
+        {
+            Database = new FirebaseClient(
+                url,
+                new FirebaseOptions
+                {
+                    AuthTokenAsyncFactory = () => Task.FromResult(token)
+                }
+            );
+            var a = Database.Child("zakhar").PostAsync(2);
+            Console.WriteLine(a.Result.Object);
+        }
+        
+        public int AddTask(long chatId, string answer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetAnswer(int taskId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetSolved(int taskId, bool solved)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<bool> GetLastStats(long chatId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetLevel(long chatId, int level)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetLevel(long chatId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- theta-bot/Program.cs
+++ theta-bot/Program.cs
@@ -6,13 +6,14 @@ namespace theta_bot
     {
         public static void Main(string[] args)
         {
-            new ThetaBot(
-                new TelegramBotClient(args[0]),
-                new SQLiteProvider(args[1]), 
-                new Level0(),
-                new Level1(),
-                new Level2() 
-                );
+            new FirebaseProvider("https://thetabot-4a6fa.firebaseio.com", "AIzaSyAIZ4KTPkWY2ZkIxOVavWCpakf82o7OcZ0");
+//            new ThetaBot(
+//                new TelegramBotClient(args[0]),
+//                new SQLiteProvider(args[1]), 
+//                new Level0(),
+//                new Level1(),
+//                new Level2() 
+//                );
         }
     }
 }
