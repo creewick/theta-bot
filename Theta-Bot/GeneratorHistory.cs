c30005105a42117ef9c6b9135b988a92ea4fc851
creewick <ilia_Y@mail.ru>
2018-12-05 20:57:14 +0500

    .gitignore & test in separate project

--- Theta-Bot/GeneratorHistory.cs
+++ Theta-Bot/GeneratorHistory.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Theta_Bot
+﻿namespace Theta_Bot
 {
     public class GeneratorHistory
     {
--- Theta-Bot/Program.cs
+++ Theta-Bot/Program.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Theta_Bot
+﻿namespace Theta_Bot
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
